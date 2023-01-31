582d95908b63135479d54ad061f2da3beb45fde6
chaehun <chaehun@uniess.co.kr>
2023-01-31 16:53:33 +0900

    최초 커밋

---
+++ src/main/java/kr/or/visitkorea/api/FrameApplication.java
@@ -0,0 +1,13 @@
+package kr.or.visitkorea.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/or/visitkorea/api/FrameApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.visitkorea.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
